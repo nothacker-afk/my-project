32ea39f15806043de087a1d0a2188638039deece
nothacker-afk <oswabrian00@gmail.com>
2026-01-01 13:07:59 +0000

    fix(routes): clean polls route and use pollController with auth

--- backend/routes/polls.js
+++ backend/routes/polls.js
@@ -1,16 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const pollController = require('../controllers/pollController');
-
-router.post('/', pollController.createPoll);
-router.get('/', pollController.listPolls);
-router.get('/:id', pollController.getPoll);
-router.post('/:id/vote', pollController.vote);
-
-module.exports = router;
-const express = require('express');
-const router = express.Router();
-const { createPoll, listPolls, getPoll, vote } = require('../controllers/pollsController');
+const { createPoll, listPolls, getPoll, vote } = require('../controllers/pollController');
 const { protect } = require('../middleware/auth');
 
 router.post('/', protect, createPoll);
